e2f452f41214a3e3cc68c27caf9cb5496384bc86
James M Snell <jasnell@gmail.com>
2016-12-24 18:21:58 -0800

    Update code and dependencies

--- qrenroll.js
+++ qrenroll.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function has_jsqrcode() {
   try {
     require('jsqrcode');
@@ -20,46 +22,43 @@ if (has) {
   const fs = require('fs');
 
   function enroll(arg, options) {
-    setImmediate(function() {
-      var qr_path = path.resolve(process.cwd(), arg);
-      if (!fs.existsSync(qr_path)) {
-        console.log('cannot read QRCode image');
-        process.exit(1);
+    var qr_path = path.resolve(process.cwd(), arg);
+    if (!fs.existsSync(qr_path)) {
+      console.error('cannot read QRCode image');
+      process.exit(-1);
+    }
+    var image = new Image();
+    image.onload = function() {
+      var result;
+      try{
+        result = qrcode.decode(image);
+        var parsed = url.parse(result);
+        var label = options.label || (unescape(parsed.pathname.substr(1)));
+        var query = qs.parse(parsed.query);
+        var secret = query.secret;
+        var opts = {};
+        if (query.issuer) opts.issuer = query.issuer;
+        en.enroll(label, secret, opts, function(err) {
+          if (err) {
+            console.error('unable to enroll');
+            process.exit(-1);
+          }
+          console.log('enrolled!');
+        });
+      } catch(e) {
+        console.error('unable to read qr code');
+        process.exit(-1);
       }
-      var image = new Image();
-      image.onload = function() {
-        var result;
-        try{
-          result = qrcode.decode(image);
-          var parsed = url.parse(result);
-          var label = options.label || (unescape(parsed.pathname.substr(1)));
-          var query = qs.parse(parsed.query);
-          var secret = query.secret;
-          var opts = {};
-          if (query.issuer) opts.issuer = query.issuer;
-          en.enroll(label, secret, opts, function(err) {
-            if (err) {
-              console.log('unable to enroll');
-              process.exit(1);
-            }
-            console.log('enrolled!');
-            process.exit(0);
-          });
-        }catch(e){
-          console.log('unable to read qr code');
-          process.exit(1);
-        }
-      };
-      image.src = qr_path;
-    });
+    };
+    image.src = qr_path;
   }
 
   module.exports = enroll;
 } else {
   module.exports = function() {
-    console.log(
+    console.error(
       'qrcode scanning not supported. please install optional dependencies');
-    process.exit(1);
+    process.exit(-1);
   };
 }
 module.exports.has_jsqrcode = has;
